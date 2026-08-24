17bb4283157a7abf2fc9878a3dae08a8a344ccad
shimataro <info@shimataro.me>
2026-08-25 05:42:42 +0900

    fix(android): apply size parameter to TooltipIconButton

--- composeApp/src/androidMain/kotlin/works/merc/keryx/app/ui/common/TooltipIconButton.android.kt
+++ composeApp/src/androidMain/kotlin/works/merc/keryx/app/ui/common/TooltipIconButton.android.kt
@@ -2,6 +2,7 @@ package works.merc.keryx.app.ui.common
 
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.PlainTooltip
@@ -36,7 +37,7 @@ actual fun TooltipIconButton(
         tooltip = { FlatTooltipContent(tooltip) },
         state = rememberTooltipState(),
     ) {
-        IconButton(onClick = onClick, modifier = modifier, enabled = enabled, content = content)
+        IconButton(onClick = onClick, modifier = modifier.size(size), enabled = enabled, content = content)
     }
 }
 
